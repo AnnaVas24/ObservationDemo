857d3fe3b2365a594a74e31eae983e4c75ce6b8a
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-07-11 14:26:47 +0300

    Initial Commit

---
+++ ObservationDemo/ObservationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationDemoApp.swift
+//  ObservationDemo
+//
+//  Created by Vasichko Anna on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ObservationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
